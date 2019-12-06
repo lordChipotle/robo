5380ea7e5b23c9fbafe1fb7f606aa579f978667d
chiforeanu <55750203+chiforeanu@users.noreply.github.com>
2019-12-06 17:55:02 +0000

    Update LineFollower.java

--- LineFollower.java
+++ LineFollower.java
@@ -20,7 +20,7 @@ public class LineFollower3{
 	private static EV3LargeRegulatedMotor motorC;
 	
 	/** The motor on the right side of the robot */
-	//private static EV3LargeRegulatedMotor motorD;
+	private static EV3LargeRegulatedMotor motorA;
 
 	/** The raw EV3 Color Sensor object */
 	private static EV3ColorSensor colorSensor;
@@ -30,43 +30,42 @@ public class LineFollower3{
 	private static float[] values; 
 	
 	/** The raw EV3 Ultrasonic Sensor object */
-	//private static EV3UltrasonicSensor ultrasonicSensor;
+	private static EV3UltrasonicSensor ultrasonicSensor;
 	
-	//SampleProvider distanceProvider;
+	private static SampleProvider distanceProvider;
 	
-	//float[] distance;
+	private static float[] distance;
 
+	private static double target; 
+	
 	public static void main(String[] args) {
 		motorB = new EV3LargeRegulatedMotor(MotorPort.B);
 		motorC = new EV3LargeRegulatedMotor(MotorPort.C);
-		//motorD = new EV3LargeRegulatedMotor(MotorPort.D);
+		motorA = new EV3LargeRegulatedMotor(MotorPort.A);
 		colorSensor = new EV3ColorSensor(SensorPort.S3);
 		intensityProvider = colorSensor.getRedMode(); 
 		values = new float[intensityProvider.sampleSize()];
-		//ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S4);
-        //ultrasonicSensor.enable(); 
-		//distanceProvider = ultrasonicSensor.getDistanceMode(); 
-		//distance = new float[distanceProvider.sampleSize()];
+		ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S4);
+        ultrasonicSensor.enable(); 
+		distanceProvider = ultrasonicSensor.getDistanceMode(); 
+		distance = new float[distanceProvider.sampleSize()];
 		
 		Button.waitForAnyPress(); 
 		
 		intensityProvider.fetchSample(values, 0);
 		
-		while(colorSensor.getColorID() != Color.BLACK) { Delay.msDelay(100); }
+		//while(colorSensor.getColorID() == 5) { Delay.msDelay(100); }
 		
 		move(); 
 	}
 	
 	private static void move() {
-		double Kp = 400; //350;  
-		double offset = 0.45; //0.38 // try less than 20 
+		double Kp = 400;   
+		double offset = 0.38; 
 		double cTurn;
 		double bTurn;
-		double Tp = 50;
-		//SampleProvider intensityProvider = colorSensor.getRedMode(); 
-		//float[] values = new float[intensityProvider.sampleSize()];
+		double Tp = 100; 
 		intensityProvider.fetchSample(values,0); 
-        //distanceProvider.fetchSample(distance,0); 
 		while(values[0] < 1){
 		  double error = values[0] - offset; 
 	      double turn = Kp * error; 
@@ -77,26 +76,43 @@ public class LineFollower3{
 		  motorC.setSpeed(new Double(cTurn).intValue());
 		  motorC.forward();
 		  intensityProvider.fetchSample(values,0);
-		 // distanceProvider.fetchSample(distance,0);
-		 // if(distance[0] < 20) { avoidObstacle(); }
-		  while(colorSensor.getColorID() !=  Color.BLACK && colorSensor.getColorID() !=  Color.WHITE) { motorB.stop(); motorC.stop(); }
+		  distanceProvider.fetchSample(distance,0);
+		  if(distance[0] < 0.10) { target = distance[0]; motorB.stop(); motorC.stop(); avoidObstacle(); }
+		  //while(colorSensor.getColorID() ==  5) { motorB.stop(); motorC.stop(); }
 		} 
 	}
 	
-	/*private static void avoidObstacle(){
-	    motorB.rotate(90); 
-	    motorC.rotate(90); 
-	    intensityProvider.fetchSample(values,0); 
-	    double target = distance[0]; 
-	    while(values[0] >= 0.80/color.getColorID() == Color.WHITE) {
+	private static void avoidObstacle(){
+	    motorB.setSpeed(50);
+	    motorC.setSpeed(50);
+	    motorB.rotate(180); 
+	    motorC.rotate(-180);
+	    motorB.backward();
+	    motorC.backward();
+	    Delay.msDelay(100);
+	    motorA.rotate(-90);
+	    motorB.forward();
+	    motorC.forward();
+	    Delay.msDelay(1000);
+	    intensityProvider.fetchSample(values,0);  
+	    while(values[0] >= 0.60) {
 		  distanceProvider.fetchSample(distance,0);
-		  if(distance[0] < target) { motorB.rotate(10); motorC.rotate(10); motorB.forward(); motorC.forward();}
-		  else if(distance[0] > target) { motorB.rotate(-10); motorC.rotate(-10); motorB.forward(); motorC.forward();}
-		  else if(distance[0] == target) { motorB.forward(); motorC.forward(); }
+		  if(distance[0] < target) { motorB.setSpeed(100); motorC.setSpeed(50); }
+		  else if(distance[0] > target) { motorB.setSpeed(50); motorC.setSpeed(100); }
+		  else if(distance[0] == target) { motorB.setSpeed(50); motorC.setSpeed(50);}
+		  motorB.forward(); motorC.forward();
 		  intensityProvider.fetchSample(values,0); 
 		}
-		motorD.rotate(-90); 
-	}*/
-	
-	
+	    motorB.stop(); 
+	    motorC.stop(); 
+	    motorA.rotate(90); 
+		motorB.setSpeed(20);
+		motorC.setSpeed(20);
+		while(values[0] < 0.20){
+		  motorB.setSpeed(50);
+		  motorC.setSpeed(10);
+		  motorB.forward();
+		  motorC.forward(); 
+		}
+	}
 }
