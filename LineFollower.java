ca070bb7226fd841d801ffe378f833278ff2d5d6
lordChipotle <36554153+lordChipotle@users.noreply.github.com>
2020-03-26 21:37:18 +0000

    Update LineFollower.java

--- LineFollower.java
+++ LineFollower.java
@@ -1,3 +1,5 @@
+
+
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.motor.UnregulatedMotor;
@@ -28,9 +30,9 @@ private  EV3GyroSensor gyro;
 /** The raw EV3 Ultrasonic Sensor object */
 private  EV3UltrasonicSensor ultrasonicSensor;
 
-private SampleProvider gyroProvider; // use this to fetch samples
-private SampleProvider colorProvider; 
-private SampleProvider distanceProvider; 
+private SampleProvider gyroProvider; // use this to fetch samples for gyro sensor
+private SampleProvider colorProvider; // fetch samples for color sensor
+private SampleProvider distanceProvider; //fetch samples for ultrasonic sensor
 
 public  void main(String[] args) {
  motorB = new UnregulatedMotor(MotorPort.B);
@@ -41,13 +43,14 @@ public  void main(String[] args) {
  gyroProvider = gyro.getAngleMode();
  colorProvider = colorSensor.getMode("color");
  distanceProvider = ultrasonicSensor.getMode("distance");
- gyro.reset();
+ gyro.reset();//reset gyro sensor reading
  move(); // start it running
 }
 private  void move() {
 	while(colorSensor.getColorID() == 5) {
 		motorB.stop();
 		motorC.stop();
+		//pause when read red stripe
 	}		
 // so long as we can keep finding the line...
 while (findLine()) { followLine(); }
@@ -55,7 +58,7 @@ while (findLine()) { followLine(); }
 
 public void tankDrive(double left, double right) {
 	if(left>100) {
-		left = 100;
+		left = 100; //setting a limit as max speed is 100 for lego motor
 	}
 	else if (left<-100) {
 		left = -100;
@@ -67,14 +70,14 @@ public void tankDrive(double left, double right) {
 		right = -100;
 	}
 	if (left>0) {
-		motorB.setPower( (int) Math.abs(right));
+		motorB.setPower( (int) Math.abs(right)); //set speed
 
 		motorB.forward();
-		if (right>0) {
+		if (right>0) { //positive means forward
 			motorC.setPower( (int) Math.abs(right));
 			motorC.forward();
 		}
-		else if (right<0) {
+		else if (right<0) {//negative means backward
 			motorC.setPower( (int) Math.abs(right));
 
 			motorC.backward();
@@ -97,17 +100,18 @@ public void tankDrive(double left, double right) {
 public void arcadeDrive(double throttleValue, double turnValue) {
     double leftMtr;
     double rightMtr;
-    leftMtr = throttleValue + turnValue;
+    leftMtr = throttleValue + turnValue; //turning towards left if turn value is positive / towards right if negative
     rightMtr = throttleValue - turnValue;
     tankDrive(leftMtr, rightMtr);
 }
 public float getM() {
 	float[] sample = new float[gyroProvider.sampleSize()];
 	while(true) 
-		  return sample[0];
+		  return sample[0]; // get gyro reading
 }
 private void turn(float degrees) {
-	double tP =0.15;
+	//kp,ki,kd for turning
+	double tP =0.15; 
 	double tI =0.01;
 	double tD = 0.01;
 	double Tintegral = 0;
@@ -130,6 +134,7 @@ private void turn(float degrees) {
 	
 
 private  void followLine(){
+	//linefollower code
  double Kp = 0.15;
  double Ki = 0.01; 
  double Kd = 0.0; 
@@ -161,6 +166,7 @@ private  void followLine(){
 }
 
 private  boolean findLine() {
+	//searching for line code
 	float[] sample = new float[colorProvider.sampleSize()];
 	sample[0] = -1;
 	while (sample[0] > 0.5) {
